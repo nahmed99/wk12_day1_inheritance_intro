23119c1125fd769cf6617bb01e56cce562d87a04
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-01 11:19:08 +0000

    Parent class added

---
+++ src/main/java/Person.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+public class Person {
+
+    private String name;
+    private String cohort;
+
+    public Person(String name, String cohort) {
+        this.name = name;
+        this.cohort = cohort;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getCohort() {
+        return this.cohort;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setCohort(String newCohort) {
+        this.cohort = newCohort;
+    }
+
+
+    public String talk(String language) {
+        return "I like " + language;
+    }
+
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+    Person person;
+
+    @Before
+    public void before() {
+        person = new Person("Kidda", "E15");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Kidda", person.getName());
+    }
+
+    @Test
+    public void hasCohort() {
+        assertEquals("E15", person.getCohort());
+    }
+
+    @Test
+    public void canChangeName(){
+        person.setName("Jane");
+        assertEquals("Jane", person.getName());
+    }
+
+    @Test
+    public void canChangeCohort(){
+        person.setCohort("G22");
+        assertEquals("G22", person.getCohort());
+    }
+
+    @Test
+    public void canTalk(){
+        assertEquals("I like Java", person.talk("Java"));
+    }
+}
